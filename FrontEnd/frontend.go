787ec4f57acaf5d952ca82b65a5ecc17ecd76c3b
Lignio <olfw@itu.dk>
2022-11-23 14:06:20 +0100

    frontend wip

--- FrontEnd/frontend.go
+++ FrontEnd/frontend.go
@@ -1,23 +1,18 @@
 package main
 
 import (
-	"bufio"
 	"context"
 	"fmt"
 	"log"
+	"net"
 	"os"
-	"strconv"
-	"strings"
-
-	// "grpc"
 
 	"google.golang.org/grpc"
 	handin "handin5.dk/uni/grpc"
 )
 
-var id int32
-
 func main() {
+
 	LOG_FILE := "./txtLog"
 	logFile, err := os.OpenFile(LOG_FILE, os.O_APPEND|os.O_RDWR|os.O_CREATE, 0644)
 	if err != nil {
@@ -26,35 +21,55 @@ func main() {
 	defer logFile.Close()
 	log.SetOutput(logFile)
 
+	ownPort := 5003
+
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
+	fe := &frontend{
+		clients: make(map[int32]handin.AuctionClient),
+		ctx:     ctx,
+	}
+
+	// Create listener tcp on port ownPort
+	go func() {
+		for {
+			list, err := net.Listen("tcp", fmt.Sprintf(":%v", ownPort))
+			if err != nil {
+				log.Fatalf("Failed to listen on port: %v", err)
+			}
+			grpcServer := grpc.NewServer()
+			handin.RegisterAuctionServer(grpcServer, fe)
+
+			go func() {
+				if err := grpcServer.Serve(list); err != nil {
+					log.Fatalf("failed to server %v", err)
+				}
+			}()
+		}
+	}()
 	var opts []grpc.DialOption
 	opts = append(opts, grpc.WithBlock(), grpc.WithInsecure())
 	var client handin.AuctionClient
+	for i := 0; i < 3; i++ {
+		port := int32(5000) + int32(i)
 
-	port := int32(5003)
-
-	conn, err := grpc.Dial(fmt.Sprintf(":%v", port), opts...)
-	if err != nil {
-		log.Fatalf("Failed to connect: %v", err)
-	}
-	client = handin.NewAuctionClient(conn)
-	// client.Connect(conn)
-	defer conn.Close()
-
-	scanner := bufio.NewScanner(os.Stdin)
-	for {
-		test := scanner.Text()
-		if strings.Contains(test, "Bid") {
-			bid, _ := strconv.Atoi(scanner.Text())
-			go sendBid(ctx, client, int32(bid))
+		conn, err := grpc.Dial(fmt.Sprintf(":%v", port), opts...)
+		if err != nil {
+			log.Fatalf("Failed to connect: %v", err)
 		}
-		if strings.Contains(test, "Result") {
-			go getResult(ctx, client)
-		}
-
+		client = handin.NewAuctionClient(conn)
+		//' client.Connect(conn)
+		defer conn.Close()
+		fe.clients[client.Id] = client
 	}
+
+}
+
+type frontend struct {
+	handin.UnimplementedAuctionServer
+	clients map[int32]handin.AuctionClient
+	ctx     context.Context
 }
 
 func sendBid(ctx context.Context, client handin.AuctionClient, bidAmount int32) {
@@ -66,15 +81,6 @@ func sendBid(ctx context.Context, client handin.AuctionClient, bidAmount int32) 
 	if err != nil {
 		log.Printf("Cannot send bid: error: %v", err)
 	}
-
-	// stream.send(&msg)
-
-	// ack, err := stream.CloseAndRecv()
-	// if err != nil {
-	// 	log.Print("Cannot send ack: %v", err)
-	// 	log.Print(ack)
-	// }
-
 }
 
 func getResult(ctx context.Context, client handin.AuctionClient) {
@@ -84,13 +90,6 @@ func getResult(ctx context.Context, client handin.AuctionClient) {
 		log.Printf("Cannot send bid: error: %v", err)
 	}
 
-	// stream.Send()
-
-	// result, err := stream.CloseAndRecv()
-	// if err != nil {
-	// 	log.Print("Cannot send ack: %v", err)
-	// 	log.Print(result)
-	// }
 	log.Printf("result %v", stream.HighestBid)
 
 }
